0fbce8696d0d441347265cc97ab9ffe5bb990355
dfsklar <dfsklar@gmail.com>
2014-07-15 17:54:58 -0700

    List of order items now available.

--- sample_getlistoforderitems.js
+++ sample_getlistoforderitems.js
@@ -4,9 +4,8 @@ var mwsOrderAPI = require('./lib/orders.js');
 
 var marketplaceId = "ATVPDKIKX0DER";
 
-var sf = new mwsOrderAPI.requests.ListOrders({"marketplaceId": marketplaceId});
-sf.params.MarketplaceId.value = marketplaceId;
-sf.params.CreatedAfter.value = "2014-07-13";
+var sf = new mwsOrderAPI.requests.ListOrderItems();
+sf.params.AmazonOrderId.value = "111-1715221-5800265";
 client.invoke(sf, function(RESULT){
   console.log("--------");
   console.log(JSON.stringify(RESULT));
