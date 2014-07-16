79dc407459499b7513b2264ed32cbe2853826578
dfsklar <dfsklar@gmail.com>
2014-07-15 18:25:29 -0700

    Sample programs

--- sample_getlistoforders.js
+++ sample_getlistoforders.js
@@ -1,11 +1,12 @@
 var mws = require('./lib/mws.js'),
-    client = new mws.Client(  ...................
+    client = new mws.Client(  .........................
 var mwsOrderAPI = require('./lib/orders.js');
 
 var marketplaceId = "ATVPDKIKX0DER";
 
-var sf = new mwsOrderAPI.requests.ListOrderItems();
-sf.params.AmazonOrderId.value = "111-1715221-5800265";
+var sf = new mwsOrderAPI.requests.ListOrders({"marketplaceId": marketplaceId});
+sf.params.MarketplaceId.value = marketplaceId;
+sf.params.CreatedAfter.value = "2014-07-13";
 client.invoke(sf, function(RESULT){
   console.log("--------");
   console.log(JSON.stringify(RESULT));
