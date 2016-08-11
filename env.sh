0749ee9505ad0e83d8abd8cdac6ff1f488583479
Pulkit Singhal <pulkit@shoppinpal.com>
2016-08-11 13:26:13 -0500

    add a test for the products API

---
+++ env.sh
@@ -0,0 +1,16 @@
+## to run from parent level directory
+# $ . ./env.sh
+## to run from within a child project directory
+# $ . ./../env.sh
+## to test if it worked
+# $ echo $MerchantId && echo $AccessKey && echo $MarketPlaceId && echo $SecretKey
+
+#synonym for SellerId
+export MerchantId=
+
+#synonym for AWSAccessKeyId
+export AccessKey=
+
+export MarketPlaceId=
+
+export SecretKey=
